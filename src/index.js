fd7639b10b8d06c2425ef628065c76bfa3291105
luke0b1000 <luke0b1000@gmail.com>
2018-07-07 22:26:36 -0400

    7x7x1 Added middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
+// import reducers from "./reducers/index"; // does this line work as above???
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import middleware from "./middleware";
+
+const store = createStore(reducer, middleware);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
---
+++ src/middleware/index.js
@@ -0,0 +1,6 @@
+import thunk from "react-thunk";
+import logger from "./logger";
+
+import { applyMiddleware } from "redux";
+
+export default applyMiddleware(thunk, logger);
