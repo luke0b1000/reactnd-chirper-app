fc5ae8451196a5e1005dd2d0022bc21e648642ed
luke0b1000 <luke0b1000@gmail.com>
2018-07-08 01:39:48 -0400

    7x11x1 likes

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,8 +1,34 @@
+import { saveLikeToggle } from "../utils/api";
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEETS = "TOGGLE_TWEETS";
 
 export function receiveTweets(tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets
+    };
+}
+
+function toggleTweets({ id, authedUser, hasLiked }) {
+    return {
+        type: TOGGLE_TWEETS,
+        id,
+        authedUser,
+        hasLiked
+    };
+}
+
+export function handleToggleTweets(info) {
+    return dispatch => {
+        dispatch(toggleTweets(info));
+
+        return saveLikeToggle(info)
+            .then("handleToggleTweets successfully")
+            .catch(err => {
+                console.warn("Error in handleToggleTweet: ", err);
+                dispatch(toggleTweets(info));
+                alert("There was an error in liking the tweet");
+            });
     };
 }
