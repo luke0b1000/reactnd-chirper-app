23e8edab6273157278fa38650fa613bc0ad37d14
luke0b1000 <luke0b1000@gmail.com>
2018-07-09 23:20:49 -0400

    7x12x4

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux;";
+import { handleAddTweet } from "../actions/tweets";
 
 class NewTweet extends Component {
     state = {
@@ -16,8 +18,9 @@ class NewTweet extends Component {
 
         const { text } = this.state;
 
-        //todo: Add tweet to store
-        console.log(text);
+        const { dispatch, id } = this.props;
+        dispatch(handleAddTweet(text, id));
+
         this.setState(() => ({
             text: ""
         }));
@@ -55,4 +58,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet;
+export default connect()(NewTweet);
